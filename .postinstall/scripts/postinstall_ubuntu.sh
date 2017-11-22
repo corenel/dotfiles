a5327b9ba1a5d17b13df7b7f0c6b41b77f825d32
corenel <xxdsox@gmail.com>
2017-11-22 20:24:10 +0800

    fix python requirements

--- .postinstall/scripts/postinstall_ubuntu.sh
+++ .postinstall/scripts/postinstall_ubuntu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # install python packages
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install -r $HOME/.postinstall/scripts/requirements.txt
+pip3 install --upgrade pip
+pip3 install -r $HOME/.postinstall/scripts/requirements.txt
 mkdir -p $HOME/.virtualenvs
 
 # apply zshrc
