f7ef9000cda46442a68e58c8d4591bc7b0835f08
corenel <xxdsox@gmail.com>
2017-12-04 16:12:19 +0800

    fix preinstall on macOS

--- .preinstall/scripts/preinstall_macos.sh
+++ .preinstall/scripts/preinstall_macos.sh
@@ -2,6 +2,16 @@
 
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# use mirror repo for HomeBrew
+cd "$(brew --repo)" || return
+git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.bash_profile
+source ~/.bash_profile
+
+# tap
 brew tap homebrew/science
 brew tap caskroom/cask
 brew tap caskroom/fonts
@@ -15,8 +25,12 @@ brew cask install xquartz \
   font-hack-nerd-font \
   xamarin-jdk android-sdk
 
-brew install zsh xclip autojump python3 python pip-completion watch cppcheck wget nvm clang gcc \
-  htop reattach-to-user-namespace ffmpeg cmake tree ctags openssh clang-format m-cli graphviz ccat \
+brew install zsh autojump \
+  python3 python pip-completion \
+  watch cppcheck wget nvm gcc \
+  htop reattach-to-user-namespace \
+  ffmpeg cmake tree ctags openssh \
+  clang-format m-cli graphviz ccat \
   gpg ag ack fzf write-good hadolint
 brew install vim --with-python3 --without-python
 brew install neovim
@@ -28,5 +42,4 @@ npm install -g csslint prettier stylelint htmlhint eslint
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - --no-check-certificate)"
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
 
