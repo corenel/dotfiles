69445a8b8aca329e3630709c3bfbff24aed55379
corenel <xxdsox@gmail.com>
2017-12-08 21:57:32 +0800

    rewrite install script

--- .preinstall/scripts/preinstall_chromeos.sh
+++ .preinstall/scripts/preinstall_chromeos.sh
@@ -6,3 +6,9 @@ crew install buildessential zsh tmux wget htop python3 fasd neovim
 curl -Ls https://goo.gl/fd3zc -o ~/Downloads/crouton && yes | \
   sudo sh ~/Downloads/crouton -t xfce \
   sudo sh ~/Downloads/crouton -n xenial -u -t xiwi
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - --no-check-certificate)"
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
--- install.sh
+++ install.sh
@@ -4,94 +4,122 @@
 
 set -e
 
-# define constant
-basedir=$HOME/.dotfiles
-repourl=https://github.com/corenel/dotfiles.git
+#################
+#  DEFINITIONS  #
+#################
 
-function symlink() {
+BASEDIR=$HOME/.dotfiles
+REPOURL=https://github.com/corenel/dotfiles.git
+
+###############
+#  FUNCTIONS  #
+###############
+
+symlink() {
   src=$1
-  dest=$2
+  dst=$2
 
-  if [ -e $dest ]; then
-    if [ -L $dest ]; then
+  if [ -e $dst ]; then
+    if [ -L $dst ]; then
       # Already symlinked -- I'll assume correctly.
       return
     else
       # Rename files with a ".old" extension.
-      echo "$dest already exists, renaming to $dest.old"
-      backup=$dest.old
+      echo "$dst already exists, renaming to $dst.old"
+      backup=$dst.old
       if [ -e $backup ]; then
         echo "Error: $backup already exists. Please delete or rename it."
         exit 1
       fi
-      mv -v $dest $backup
+      mv -v $dst $backup
     fi
   fi
-  ln -sf $src $dest
+  ln -sf $src $dst
 }
 
-# check git
-if ! which git >/dev/null ; then
-  echo "Error: git is not installed"
-  exit 1
-fi
+check_os() {
+  while true; do
+    echo -e >&2 "Installing dotfiles for:\n\t[0] macOS\n\t[1] Ubuntu\n\t[2] ChromeOS"
+    read -r -p "(Choose 0, 1 or 2):" opt
+    case $opt in
+      0)
+        echo "macos"
+        break
+        ;;
+      1)
+        echo "ubuntu"
+        break
+        ;;
+      2)
+        echo "chromeos"
+        break
+        ;;
+      *)
+        echo >&2 "Please choose 0, 1 or 2"
+        ;;
+    esac
+  done
+}
 
-# clone or update git repo
-if [ -d $basedir/.git ]; then
-  echo "Updating dotfiles using existing git..."
-  cd $basedir
-  git pull --quiet --rebase origin master
-  git submodule update --init --recursive --remote
-else
-  echo "Checking out dotfiles using git..."
-  rm -rf $basedir
-  git clone --quiet --depth=1 $repourl $basedir
-  git submodule update --init --recursive --remote
-fi
-
-# create symbol links from $basedir to $HOME
-cd $basedir
-echo "Creating symlinks..."
-for path in .* ; do
-  case $path in
-    .|..|.git|.gitignore|.gitignore.example|.gitconfig.example|.ysvim)
-      continue
-      ;;
-    *)
-      symlink $basedir/$path $HOME/$path
-      ;;
-  esac
-done
-
-# init global gitconfig and gitignore
-cp -v .gitconfig.example "$HOME/.gitconfig"
-cp -v .gitignore.example "$HOME/.gitignore"
-
-# install ysvim
-cd $basedir/.ysvim
-./install.sh
-
-# install/update vim plugins
-# symlink $basedir/.vim/vimrc $HOME/.vimrc
-# echo "Setting up vim plugins..."
-# mkdir -p $HOME/.vim/files/info
-# .vim/update.sh
-
-# post-install for specific os
-postinstall=$HOME/.postinstall
-if [ -e $postinstall ]; then
-  echo "Running post-install for os-specific dotfiles"
-  # check os type
-  if [ "$(uname)" = "Darwin" ]; then
-    oscustom=macos
-  elif [ "$(uname)" = "Linux" ]; then
-    oscustom=ubuntu
-  else
-    echo "unsupported os type."
-    return
+check_git() {
+  if ! which git >/dev/null ; then
+    echo "Error: git is not installed"
+    exit 1
   fi
+}
 
-  cd $postinstall/$oscustom
+check_repo() {
+  # clone or update git repo
+  if [ -d $BASEDIR/.git ]; then
+    echo "Updating dotfiles using existing git..."
+    cd $BASEDIR
+    git pull --quiet --rebase origin master
+    git submodule update --init --recursive --remote
+  else
+    echo "Checking out dotfiles using git..."
+    rm -rf $BASEDIR
+    git clone --quiet --depth=1 $REPOURL $BASEDIR
+    git submodule update --init --recursive --remote
+  fi
+}
+
+link_dotfiles() {
+  # create symbol links from $basedir to $HOME
+  cd $BASEDIR
+  echo "Creating symlinks..."
+  for path in .* ; do
+    case $path in
+      .|..|.git|.gitignore|.gitignore.example|.gitconfig.example|.ysvim)
+        continue
+        ;;
+      *)
+        symlink $BASEDIR/$path $HOME/$path
+        ;;
+    esac
+  done
+
+  # init global gitconfig and gitignore
+  cp -v .gitconfig.example "$HOME/.gitconfig"
+  cp -v .gitignore.example "$HOME/.gitignore"
+}
+
+
+install_ysvim() {
+  cd $BASEDIR/.ysvim
+  ./install.sh
+}
+
+install_orig_vim() {
+  symlink $BASEDIR/.vim/vimrc $HOME/.vimrc
+  echo "Setting up vim plugins..."
+  mkdir -p $HOME/.vim/files/info
+  $HOME/.vim/update.sh
+}
+
+post_install() {
+  # post-install for specific os
+  postinstall=BASEDIR/.postinstall/$1
+  cd $postinstall
   # symlink os custom
   for path in .* ; do
     case $path in
@@ -100,20 +128,27 @@ if [ -e $postinstall ]; then
         ;;
       .config)
         echo "rsync .config"
-        rsync -rtv $postinstall/$oscustom/$path/ $HOME/.config
+        rsync -rtv "$postinstall/$path/" "$HOME/.config"
         ;;
       *)
-        symlink $postinstall/$oscustom/$path $HOME/$path
+        symlink "$postinstall/$path" "$HOME/$path"
         ;;
     esac
   done
 
   # execute post-install script
-  $postinstall/scripts/postinstall_$oscustom.sh
-else
-  echo "No post install script found. Optionally create one at $postinstall"
-fi
+  "$postinstall/scripts/postinstall_$oscustom.sh"
+}
 
-echo "Done."
+#################
+#  MAIN SCRIPT  #
+#################
+
+OSCUSTOM=$(check_os)
+check_git
+check_repo
+link_dotfiles
+install_ysvim
+postinstall $OSCUSTOM
 
 } # This ensures the entire script is downloaded.
