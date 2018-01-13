2c754b734509c46a698c42419ff1d6c0d0ce9108
corenel <xxdsox@gmail.com>
2018-01-13 22:42:54 +0800

    add texlive for chromeos

--- .postinstall/scripts/postinstall_chromeos.sh
+++ .postinstall/scripts/postinstall_chromeos.sh
@@ -5,5 +5,15 @@ pip3 install --upgrade pip
 pip3 install -r $HOME/.postinstall/scripts/requirements.txt
 mkdir -p $HOME/.virtualenvs
 
+# install texlive
+crew install texlive
+tlmgr option repository https://mirrors.ustc.edu.cn/CTAN/systems/texlive/tlnet/
+tlmgr update --self
+tlmgr update --all
+tlmgr install latexmk xetex beamer l3packages l3kernel fontspec \ 
+  mathtools tex4ht doublestroke \
+  ctex xecjk fandol \
+  multirow caption subfig booktabs enumitem 
+
 # apply zshrc
 source $HOME/.zshrc
