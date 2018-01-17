07d31682ff7fd03f9197ec1fd24e8ecf0a4c2189
corenel <xxdsox@gmail.com>
2018-01-17 13:17:20 +0800

    add node for chromeos

--- .preinstall/scripts/preinstall_chromeos.sh
+++ .preinstall/scripts/preinstall_chromeos.sh
@@ -1,6 +1,7 @@
 # install chromebrew
 curl -Ls git.io/vddgY -o install.sh && yes | bash install.sh
-crew install buildessential zsh tmux wget htop python3 fasd neovim
+crew install buildessential zsh tmux wget htop python3 fasd neovim node
+ln -sf /usr/local/lib64/nodebrew/completions/zsh/_nodebrew /usr/local/share/zsh/site-functions/
 
 # install crouton
 curl -Ls https://goo.gl/fd3zc -o ~/Downloads/crouton && yes | \
